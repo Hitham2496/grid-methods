f876fceeb6c53e920a3fd1035fcfa24232178497
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-11-26 15:26:37 +0000

    Add template for jobs

---
+++ JobTemplate/run_job.py
@@ -0,0 +1,58 @@
+"""
+Runs a multithreaded HEJ+Pythia job on a single grid node.
+"""
+import argparse
+import os
+import time
+import multiprocessing
+from .job import Job
+
+
+def parse():
+    """
+    Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(description = "Usage: python run_multi.py -u user_name -j job_number -p runs_per_job -e events -b base_dir -o grid_output_dir")
+    parser.add_argument('--user_name', '-u', nargs = 1, type = str)
+    parser.add_argument('--job_number', '-j', nargs = 1, type = int, default = 1)
+    parser.add_argument('--processes', '-p', nargs = 1, type = int, default = 1)
+    parser.add_argument('--events', '-e', nargs = 1, type = int, default = 100)
+    parser.add_argument('--base_dir', '-b', nargs = 1, type = str, default = os.getcwd())
+    parser.add_argument('--output', '-o', nargs = 1, type = str)
+    return parser.parse_args()
+
+
+def main():
+    """
+    Run multiple HEJ+Pythia jobs per submission node.
+    """
+    args = parse()
+
+    t0 = time.time()
+    grid_job = Job(args.user_name[0], args.job_number[0], args.base_dir[0], args.rivet_dir[0], args.output[0])
+    grid_job.set_env()
+
+    if args.threads[0] > 4:
+        raise(ValueError("Maximum number of threads is 4 per node."))
+
+    t1 = time.time()
+    jobs = []
+    for number in range(args.processes[0]):
+        p = multiprocessing.Process(target = grid_job.run_job, args = (number, args.events[0]))
+        jobs.append(p)
+
+    for job in jobs:
+        job.start()
+    
+    for job in jobs:
+        job.join()
+
+    t2 = time.time()
+
+    print("Environment setting time %s(s)" % (t1 - t0))
+    print("Execution time %s(s)" % (t2 - t1))
+    print("Total time %s(s)" % (t2 - t0))
+
+
+if __name__ == """__main__""":
+    main()
