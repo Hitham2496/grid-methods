ca8ddeab31deb97173edc59cad579098e46b72ee
Hitham Hassan <hitham.t.hassan@durham.ac.uk>
2022-12-09 09:58:41 +0000

    Update labelling of arguments in run_hejpythia and hejpythia_manager

--- src/HejPythiaJob/hejpythia_manager.py
+++ src/HejPythiaJob/hejpythia_manager.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import os
-from .hejpythia_job import HejPythiaJob, HejPythiaMerger
+from hejpythia_job import HejPythiaJob, HejPythiaMerger
 import argparse
 
 
-def make_job_file(user_name, job_number, events, threads, base_dir, rivet_dir, output_dir, name):
+def make_job_file(user_name, job_number, events, processes, base_dir, rivet_dir, output_dir, name):
     """
     Creates xrsl submission file given:
         job_number : int between n_min and n_max (inclusive)
         events : int number of events per run
-        threads : int number of runs per core < 4
+        processes : int number of runs per submission < 4
         base_dir : base directory containing run configuration files
         rivet_dir : directory containing rivet analyses
         output_dir : directory on grid storage for output, with protocol
@@ -17,20 +17,20 @@ def make_job_file(user_name, job_number, events, threads, base_dir, rivet_dir, o
     """
     print("Writing job%s.jdl" % (job_number))
     cmd = """echo "&(executable = '%s')\n""" % (name)
-    cmd += """(arguments = '-u' '%s' '-j' '%s' '-t' '%s' '-e' '%s' '-b' '%s' '-r' '%s' '-o' '%s')\n""" % (user_name, job_number, threads, events, base_dir, rivet_dir, output_dir)
+    cmd += """(arguments = '-u' '%s' '-j' '%s' '-p' '%s' '-e' '%s' '-b' '%s' '-r' '%s' '-o' '%s')\n""" % (user_name, job_number, processes, events, base_dir, rivet_dir, output_dir)
     cmd += """(jobname = %s.%s)\n""" % (name, job_number)
     cmd += """(stdout = 'stdout')\n(stderr = 'stderr')\n(gmlog = 'job%s.log')\n""" % (job_number)
-    cmd += """(count = '%s')\n(countpernode = '%s')" """ % (threads, threads)
+    cmd += """(count = '%s')\n(countpernode = '%s')" """ % (processes, processes)
     cmd += """> job%s.jdl""" % (job_number)
     os.system(cmd)
 
 
 def run(args):
     """
-    Submits n_max - n_min + 1 multithreaded xrsl job scripts to the grid.
+    Submits n_max - n_min + 1 multiprocessed xrsl job scripts to the grid.
     """
     for idx in range(args["n_min"], args["n_max"] + 1):
-        make_job_file(args["user_name"], idx, args["events"], args["threads"],
+        make_job_file(args["user_name"], idx, args["events"], args["processes"],
                       args["base_dir"], args["rivet_dir"],
                       args["output_dir"], args["job_name"])
 
@@ -86,7 +86,7 @@ if __name__ == """__main__""":
         n_min      : int starting job number
         n_max      : int final job number
         events     : int number of events per run
-        threads    : int number of runs per core < 4
+        processes  : int number of runs per core < 4
         user_name  : user name on gridui and dpm storage
         base_dir   : base directory containing run configuration files
         rivet_dir  : directory containing rivet analyses
@@ -98,7 +98,7 @@ if __name__ == """__main__""":
            "n_min"      : 100,
            "n_max"      : 100,
            "events"     : 1000,
-           "threads"    : 4,
+           "processes"  : 4,
            "user_name"  : "hhassan",
            "job_name"   : "run_hejpythia.py",
            "base_dir"   : "/mt/home/hhassan/Projects/HEJ_PYTHIA/pythia_merging/Setup/7TeV/7TeV-40GeV-60GeV-Optimised-R0pt4/2j_HT2_7TeV/",
--- src/HejPythiaJob/run_hejpythia.py
+++ src/HejPythiaJob/run_hejpythia.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 """
-Runs a multithreaded HEJ+Pythia job on a single grid node.
+Runs a multiprocessed HEJ+Pythia job on a single grid node.
 """
 import argparse
 import os
@@ -11,11 +12,19 @@ from .hejpythia_job import HejPythiaJob
 def parse():
     """
     Parse command line arguments.
+        user_name : str user handle on gridui and dpm storage
+        job_number : int identifying submission
+        runs_per_job : int number of runs per submission < 4
+        events : int number of events per run
+        base_dir : base directory containing run configuration files
+        rivet_dir : directory containing rivet analyses
+        grid_output_dir : directory on grid storage for output, with protocol
+        name : job name
     """
-    parser = argparse.ArgumentParser(description = "Usage: python run_hejpythia.py -u user_name -j job_number -t runs_per_job -e events -b base_dir -r rivet_dir -o grid_output_dir")
+    parser = argparse.ArgumentParser(description = "Usage: python run_hejpythia.py -u user_name -j job_number -p runs_per_job -e events -b base_dir -r rivet_dir -o grid_output_dir")
     parser.add_argument('--user_name', '-u', nargs = 1, type = str)
     parser.add_argument('--job_number', '-j', nargs = 1, type = int, default = 1)
-    parser.add_argument('--threads', '-t', nargs = 1, type = int, default = 1)
+    parser.add_argument('--processes', '-p', nargs = 1, type = int, default = 1)
     parser.add_argument('--events', '-e', nargs = 1, type = int, default = 100)
     parser.add_argument('--base_dir', '-b', nargs = 1, type = str, default = os.getcwd())
     parser.add_argument('--rivet_dir', '-r', nargs = 1, type = str, default = os.getcwd())
@@ -33,12 +42,12 @@ def main():
     hejpythia = HejPythiaJob(args.user_name[0], args.job_number[0], args.base_dir[0], args.rivet_dir[0], args.output[0])
     hejpythia.set_env()
 
-    if args.threads[0] > 4:
-        raise(ValueError("Maximum number of threads is 4 per node."))
+    if args.processes[0] > 4:
+        raise(ValueError("Maximum number of processes is 4 per node."))
 
     t1 = time.time()
     jobs = []
-    for number in range(args.threads[0]):
+    for number in range(args.processes[0]):
         p = multiprocessing.Process(target = hejpythia.run_job, args = (number, args.events[0]))
         jobs.append(p)
 
