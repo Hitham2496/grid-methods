e1516849e6926cdb82ac6d82e11a4fa35fa33d97
hhassan <hitham.t.hassan@durham.ac.uk>
2023-03-15 17:40:12 +0000

    Add new methods for HEJFOG initiated HEJ+Pythia

---
+++ src/HejFogPythiaJob/hejfogpythia_manager.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+import os
+from run_hejfogpythia import HejFogPythiaJob, HejFogPythiaMerger
+import argparse
+
+
+def make_job_file(user_name, job_number, events, processes, base_dir, rivet_dir, output_dir, name):
+    """
+    Creates xrsl submission file given:
+        job_number : int between n_min and n_max (inclusive)
+        events : int number of events per run
+        processes : int number of runs per submission < 4
+        base_dir : base directory containing run configuration files
+        rivet_dir : directory containing rivet analyses
+        output_dir : directory on grid storage for output, with protocol
+        name : job name
+    """
+    print("Writing job%s.jdl" % (job_number))
+    cmd = """echo "&(executable = '%s')\n""" % (name)
+    cmd += """(arguments = '-u' '%s' '-j' '%s' '-p' '%s' '-e' '%s' '-b' '%s' '-r' '%s' '-o' '%s')\n""" % (user_name, job_number, processes, events, base_dir, rivet_dir, output_dir)
+    cmd += """(jobname = %s.%s)\n""" % (name, job_number)
+    cmd += """(stdout = 'stdout')\n(stderr = 'stderr')\n(gmlog = 'job%s.log')\n""" % (job_number)
+    cmd += """(count = '%s')\n(countpernode = '%s')" """ % (processes, processes)
+    cmd += """> job%s.jdl""" % (job_number)
+    os.system(cmd)
+
+
+def run(args, write_only = False):
+    """
+    Submits n_max - n_min + 1 multiprocessed xrsl job scripts to the grid
+    unless write_only is set --- then only xrsl input files are written.
+    """
+    for idx in range(args["n_min"], args["n_max"] + 1):
+        make_job_file(args["user_name"], idx, args["events"], args["processes"],
+                      args["base_dir"], args["rivet_dir"],
+                      args["output_dir"], args["job_name"])
+
+        if not write_only:
+            if (idx%2) == 0:
+                cmd = "arcsub --direct -c ce1.dur.scotgrid.ac.uk -j ./multijobs.dat job%s.jdl &" % (idx)
+                os.system(cmd)
+
+            # Sleep after every other job submission.
+            else:
+                cmd = "arcsub --direct -c ce1.dur.scotgrid.ac.uk -j ./multijobs.dat job%s.jdl &" % (idx)
+                os.system(cmd)
+                cmd = "sleep 0.2"
+                os.system(cmd)
+
+    cmd = "sleep 0.5"
+    os.system(cmd)
+
+    if not write_only:
+        cmd = "rm *jdl"
+        os.system(cmd)
+
+
+def main(args):
+    """
+    Main method for manager functionality.
+    """
+    parser = argparse.ArgumentParser(description = "Usage: python hejfogpythia_manager.py [-w] [--write] -r [--run] -s [-status] -f [--finalise] -m [--merge] -c [--clean] -k [--kill]")
+    parser.add_argument('--write', '-w', action = "store_true")
+    parser.add_argument('--run', '-r', action = "store_true")
+    parser.add_argument('--status', '-s', action = "store_true")
+    parser.add_argument('--finalise', '-f', action = "store_true")
+    parser.add_argument('--merge', '-m', action = "store_true")
+    parser.add_argument('--clean', '-c', action = "store_true")
+    parser.add_argument('--kill', '-k', action = "store_true")
+    manager_args = parser.parse_args()
+
+    if manager_args.run or manager_args.write:
+         run(args, manager_args.write)
+         return
+
+    if manager_args.status:
+         print("Writing job statuses to logfile.txt")
+         os.system("rm logfile.txt")
+         job_statuses = os.popen("arcstat -j multijobs.dat").read()
+
+         n_running = os.popen("arcstat -j multijobs.dat | grep -i 'Running' | wc -l").read()
+         n_finished = os.popen("arcstat -j multijobs.dat | grep -i 'Finished' | wc -l").read()
+         n_finishing = os.popen("arcstat -j multijobs.dat | grep -i 'Finishing' | wc -l").read()
+         n_failed = os.popen("arcstat -j multijobs.dat | grep -i 'Failed' | wc -l").read()
+         n_queuing = os.popen("arcstat -j multijobs.dat | grep -i 'Queuing' | wc -l").read()
+         n_missing = os.popen("arcstat -j multijobs.dat | grep -i 'Waiting' | wc -l").read()
+         n_tot = int(n_running) + int(n_finished) + int(n_finishing) + int(n_failed) + int(n_queuing) + int(n_missing)
+
+         with open("logfile.txt", "a") as logfile:
+             logfile.write("=" * 80 + "\n")
+             logfile.write("-" * 32 + " JOB INFORMATION " + "-" * 31 + "\n")
+             logfile.write("=" * 80 + "\n")
+             logfile.write(job_statuses)
+             logfile.write("\n" + "=" * 80 + "\n")
+             logfile.write("-" * 30 + " SUMMARY INFORMATION " + "-" * 29 + "\n")
+             logfile.write("=" * 80 + "\n")
+             logfile.write("Total jobs: %s\n" % str(n_tot))
+             logfile.write("Number of running jobs: %s\n" % str(n_running))
+             logfile.write("Number of finished jobs: %s\n" % str(n_finished))
+             logfile.write("Number of finishing jobs: %s\n" % str(n_finishing))
+             logfile.write("Number of failed jobs: %s\n" % str(n_failed))
+             logfile.write("Number of queuing jobs: %s\n" % str(n_queuing))
+             logfile.write("Number of missing jobs: %s\n" % str(n_missing)) 
+
+         return
+
+    if manager_args.clean:
+        os.system("arcclean -j multijobs.dat")
+        return
+
+    if manager_args.kill:
+        os.system("arckill -j multijobs.dat")
+        return
+
+    merger = HejFogPythiaMerger(args["user_name"], args["output_dir"])
+    if manager_args.finalise:
+        merger.copy_files()
+        os.system("arcclean -j multijobs.dat")
+        return
+
+    if manager_args.merge:
+        merger.merge_output()
+        return
+    
+
+if __name__ == """__main__""":
+    """
+    Alter the values in 'args' to produce appropriate configurations:
+        n_min      : int starting job number
+        n_max      : int final job number
+        events     : int number of events per run
+        processes  : int number of runs per core < 4
+        user_name  : user name on gridui and dpm storage
+        base_dir   : base directory containing run configuration files
+        rivet_dir  : directory containing rivet analyses
+        output_dir : directory on grid storage for output, with protocol
+        name : job name
+    """
+
+    args = {
+           "n_min"      : 1,
+           "n_max"      : 100,
+           "events"     : 100000,
+           "processes"  : 4,
+           "user_name"  : "hhassan",
+           "job_name"   : "/mt/home/hhassan/HEP-Tools/grid-methods/src/HejFogPythiaJob/run_hejfogpythia.py",
+           "base_dir"   : "/mt/home/hhassan/Projects/HEJ_PYTHIA/pythia_merging/Setup/7TeV/7TeV-20GeV-R06/2j_HT2_7TeV/",
+           "rivet_dir"  : "/mt/home/hhassan/Projects/HEJ_PYTHIA/pythia_merging/rivet",
+           "output_dir" : "gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/hhassan/pythia_merging/azimuthal-20GeV-2jet-single-run",
+    }
+
+    main(args)
---
+++ src/HejFogPythiaJob/run_hejfogpythia.py
@@ -0,0 +1,415 @@
+#!/usr/bin/env python
+"""
+Runs a multiprocessed HEJ+Pythia (with HEJFOG as the LO generator) job on a single
+grid node.
+"""
+import argparse
+import os
+import time
+import multiprocessing
+
+
+class HejFogPythiaJob(): 
+
+
+    def __init__(self, user_name, job_number, base_dir, rivet_dir, output_dir):
+        """
+        Initialises a (HEJFOG-initiated) HEJ+Pythia run given:
+            user_name : str user name for gridui and dpm grid storage
+            job_number : index of the submission
+            base_dir : base directory for input files
+            rivet_dir : path for compiled rivet analysis libraries, and PDFs
+            output_dir : output directory on grid storage server, with protocol
+        """
+        self.user_name = str(user_name)
+        self.job_number = int(job_number)
+        self.base_dir = str(base_dir)
+        self.rivet_dir = str(rivet_dir)
+        self.output_dir = str(output_dir)
+
+
+    def __del__(self):
+        """
+        Cleanly deletes excess files on destruction.
+        """
+        self.clean_job()
+
+
+    def set_env(self):
+        """
+        Sets the environment for a HEJ+Pythia run by downloading HEJ, HEJ_Pythia and
+        rivet analyses and setting $PATH and $LD_LIBRARY_PATH
+        and $RIVET_ANALYSIS_PATH.
+        """
+        print("Setting environment for HEJ+Pythia run")
+        os.system("date")
+        cmd = "source /mt/home/%s/.bashrc" % (self.user_name)
+        os.system(cmd)
+        os.system("source /cvmfs/pheno.egi.eu/HEJ/HEJ_env.sh")
+        os.environ["MYPROXY_SERVER"] = "myproxy.gridpp.rl.ac.uk"
+
+        print("Downloading HEJ.tar.gz from grid storage")
+        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/HEJ/HEJ.tar.gz ./ -f" % self.user_name
+        os.system(cmd)
+        print("untarring HEJ.tar.gz")
+        os.system("tar -xzf HEJ.tar.gz")
+        os.system("rm HEJ.tar.gz")
+ 
+        print("Setting environment for HEJ run")
+        self.set_hej_env()
+        os.environ["LD_LIBRARY_PATH"] = "%s/HEJ/lib:%s" % (str(os.getcwd()), str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["PATH"] = "%s/HEJ/bin:%s" % (str(os.getcwd()), str(os.environ.get("PATH",'')))
+
+        print("Downloading Pythia.tar.gz from grid storage")
+        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/Pythia/Pythia.tar.gz ./ -f" % self.user_name
+        os.system(cmd)
+        print("untarring Pythia.tar.gz")
+        os.system("tar -xzf Pythia.tar.gz")
+        os.system("rm -f Pythia.tar.gz")
+        
+        print("Downloading HEJ_pythia.tar.gz from grid storage")
+        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/HEJ_pythia/HEJ_pythia.tar.gz ./ -f" % self.user_name
+        os.system(cmd)
+        print("untarring HEJ_pythia.tar.gz")
+        os.system("tar -xzf HEJ_pythia.tar.gz")
+        os.system("rm HEJ_pythia.tar.gz")
+
+        print("Setting environment for HEJ_Pythia")
+        os.environ["PATH"] = "%s/HEJ_pythia/bin:%s" % (str(os.getcwd()), str(os.environ.get("PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "%s/Pythia/lib:%s" % (str(os.getcwd()), str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "%s/HEJ_pythia/lib:%s" % (str(os.getcwd()), str(os.environ.get("LD_LIBRARY_PATH",'')))
+
+        print("Environment set at:")
+        os.system("date")
+
+
+    def set_hej_env(self):
+        """
+        Sets the HEJ environment
+        """
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/boost/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/gcc_9/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/gcc_9/lib64/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/CLHEP/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/fastjet/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/LHAPDF/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/QCDloop/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/rivet/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/yaml-cpp/lib/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        os.environ["LD_LIBRARY_PATH"] = "/cvmfs/pheno.egi.eu/HEJ/HepMC3/lib64/:%s" % (str(os.environ.get("LD_LIBRARY_PATH",'')))
+        
+
+    def get_unique_seed(self, run_number):
+        """
+        Generates a unique integer RNG seed with Cantor's pairing function.
+        """
+        return int(0.5 * (self.job_number + int(run_number)) * (self.job_number + int(run_number) + 1) + int(run_number))
+
+
+    def run_job(self, run_number, events):
+        """
+        The main loop for the HEJ+Pythia run given the run index on the current node
+        and a number of events.
+        """
+        # TODO: Don't hardcode names of runfiles (even though they are standard)
+        seed = self.get_unique_seed(run_number)
+        cmd = "cp -r %s/configFOG.yml %s/config.yml %s/hej_merging.cmnd ." % (str(self.base_dir), str(self.base_dir), str(self.base_dir))
+        os.system(cmd)
+
+        # Modify HEJFOG input parameter seeds
+        cmd = "cp configFOG.yml configFOG_%s.yml" % (str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/events:.*/events: %s/g' configFOG_%s.yml" % (str(events), str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/seed:.*/seed: %s/g' configFOG_%s.yml" % (str(seed), str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/output:.*HEJ.*/output: HEJFOG_%s/g' configFOG_%s.yml" % (str(seed), str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/.*lhe/  - HEJFOG_%s.lhe/g' configFOG_%s.yml" % (str(seed), str(seed))
+        os.system(cmd)
+
+        # Run HEJFOG
+        print("Starting HEJFOG run at:")
+        os.system("date")
+        cmd = "HEJFOG configFOG_%s.yml" % (str(seed), str(seed))
+        os.system(cmd)
+        print("HEJFOG finished running at:")
+        os.system("date")
+
+        # Modify HEJ input parameter seeds
+        cmd = "cp config.yml config_%s.yml" % (str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/seed:.*/seed: %s/g' config_%s.yml" % (str(seed), str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/output:.*HEJ.*/output: HEJ_%s/g' config_%s.yml" % (str(seed), str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/.*lhe/  - HEJ_%s.lhe/g' config_%s.yml" % (str(seed), str(seed))
+        os.system(cmd)
+
+        # Run HEJ
+        print("Starting HEJ run at:")
+        os.system("date")
+        cmd = "HEJ config_%s.yml HEJFOG_%s.lhe" % (str(seed), str(seed))
+        os.system(cmd)
+        print("HEJ finished running at:")
+        os.system("date")
+
+        # Modify HEJ+Pythia parameter seeds
+        cmd = "cp hej_merging.cmnd hej_merging_%s.cmnd" % (str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/Random:seed.*=.*/Random:seed = %s/g' hej_merging_%s.cmnd" % (str(seed), str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/rivet:output.*=.*/rivet:output = HEJmerging_%s.yoda/g' hej_merging_%s.cmnd" % (str(seed), str(seed))
+        os.system(cmd)
+        cmd = "sed -i 's/Merging:HEJconfigPath.*=.*/Merging:HEJconfigPath = config_%s.yml/g' hej_merging_%s.cmnd" % (str(seed), str(seed))
+        os.system(cmd)
+
+        # Run HEJ+Pythia
+        print("Starting HEJ+Pythia run at:")
+        os.system("date")
+        cmd = "HEJ_Pythia hej_merging_%s.cmnd HEJ_%s.lhe" % (str(seed), str(seed))
+        os.system(cmd)
+        print("HEJ+Pythia finished running at:")
+        os.system("date")
+
+        self.print_info()
+        self.save_results(seed)
+
+
+    def save_results(self, seed):
+        """
+        Copies the analysis output files and input cards to the grid storage.
+        """
+        # Compress the output into one tarball
+        cmd = "tar -czvf hej_pythia_output%s.tar.gz *%s*.yoda *%s.cmnd *%s.yml" % (str(seed), str(seed), str(seed), str(seed))
+        os.system(cmd)
+
+        # Copy the tarball of results to the grid storage
+        cmd = "gfal-copy hej_pythia_output%s.tar.gz %s -f" % (str(seed), str(self.output_dir))
+        os.system(cmd)
+
+
+    def clean_job(self):
+        """
+        Removes the remaining files.
+        """
+        os.system("rm *gz *yml *dat *yoda *cmnd *tex *lhe* Results* -r Process Sherpa HEJ HEJ_pythia lib bin include share Pythia Status* -f")
+
+
+    def print_info(self):
+        """
+        Prints information from the grid node used.
+        """
+        print("Disk used")
+        os.system("du -ksh")
+        print("File list")
+        os.system("ls -lR")
+        print("cpuinfo")
+        os.system("cat /proc/cpuinfo")
+
+
+
+class HejFogPythiaMerger():
+
+
+    def __init__(self, user_name, grid_output_dir, prune=False, prune_script="yodastats"):
+        """
+        Initialises merger for output files given:
+            user_name       : user name for gridui and dpm grid storage
+            grid_output_dir : location of output files on grid storage
+            prune           : optional bool to prune the output data
+            prune_script    : name of C/C++ script to prune yoda files
+        """
+        self.user_name = str(user_name)
+        self.grid_output_dir = str(grid_output_dir)
+        self.prune = bool(prune)
+        if self.prune:
+            self.prune_script = prune_script
+
+        addendum = os.path.basename(os.path.normpath(grid_output_dir))
+        self.scratch_dir = "/scratch/%s/tmp_output_%s" % (str(user_name), str(addendum))
+        cmd = "mkdir %s" % self.scratch_dir
+        os.system(cmd)
+
+
+    def copy_files(self):
+        """
+        Copies grid output files to scratch dir.
+        """
+        print("Copying output to scratch")
+        cmd = "gfal-copy -f -r %s %s" % (self.grid_output_dir, self.scratch_dir)
+        os.system(cmd)
+        os.system("mkdir results")
+        os.system("mkdir results/lo-output")
+        os.system("mkdir results/hej-output")
+        os.system("mkdir results/hej-pythia-output")
+
+        print("Organising output into categories of runs")
+        files = os.listdir(self.scratch_dir)
+        print(files)
+        with multiprocessing.Pool() as pool:
+            # Use multiprocessing to organise output in parallel
+            pool.map(self.organise_single, files)
+
+        os.system("rm tmp_logfile")
+
+
+    def organise_single(self, filename):
+        """
+        Organise the tarball of results named 'filename'.
+        """
+        # TODO: Clean output for log file (e.g. mark with process ID)
+        cmd = "tar -xzf %s/%s >> tmp_logfile 2>&1" % (self.scratch_dir, filename)
+        os.system(cmd)
+        cmd = "mv LO*yoda results/lo-output >> tmp_logfile 2>&1"
+        os.system(cmd)
+        cmd = "mv HEJ_*yoda results/hej-output >> tmp_logfile 2>&1"
+        os.system(cmd)
+        cmd = "mv HEJmerging_*yoda results/hej-pythia-output >> tmp_logfile 2>&1"
+        os.system(cmd)
+        cmd = "rm *yoda *cmnd *yml >> tmp_logfile 2>&1"
+        os.system(cmd)
+
+
+    def merge_output(self, with_variations = True):
+        """
+        Prunes (if set) and merges output files.
+        """
+        if self.prune:
+            self.prune_output()
+
+        # Merge LO results
+        print("Merging LO yoda files")
+        cmd = "mkdir results/merged"
+        os.system(cmd)
+        if with_variations:
+            cmd = "yodamerge results/lo-output/HEJFOG*.MuR2_MuF2_* -o results/merged/LO-MUR2-MUF2.yoda"
+            os.system(cmd)
+            # cmd = "yodamerge results/lo-output/HEJFOG*.MuR1_MuF2_* -o results/merged/LO-MUR1-MUF2.yoda"
+            # os.system(cmd)
+            # cmd = "yodamerge results/lo-output/HEJFOG*.MuR2_MuF1_* -o results/merged/LO-MUR2-MUF1.yoda"
+            # os.system(cmd)
+            # cmd = "yodamerge results/lo-output/HEJFOG*.MuR0.5_MuF1_* -o results/merged/LO-MUR0.5-MUF1.yoda"
+            # os.system(cmd)
+            # cmd = "yodamerge results/lo-output/HEJFOG*.MuR1_MuF0.5_* -o results/merged/LO-MUR1-MUF0.5.yoda"
+            # os.system(cmd)
+            cmd = "yodamerge results/lo-output/HEJFOG*.MuR0.5_MuF0.5_* -o results/merged/LO-MUR0.5-MUF0.5.yoda"
+            os.system(cmd)
+        
+        cmd = "rm results/lo-output/HEJFOG*Mu*"
+        os.system(cmd)
+        cmd = "yodamerge results/lo-output/HEJFOG* -o results/merged/LO.yoda"
+        os.system(cmd)
+        cmd = "rm -r results/lo-output"
+        os.system(cmd)
+        print("LO yoda files merged")
+
+        # Merge HEJ results
+        print("Merging HEJ yoda files")
+        if with_variations:
+            cmd = "yodamerge results/hej-output/HEJ*MuR2_MuF2* -o results/merged/HEJ-MUR2-MUF2.yoda"
+            os.system(cmd)
+            # cmd = "yodamerge results/hej-output/HEJ*MuR1_MuF2* -o results/merged/HEJ-MUR1-MUF2.yoda"
+            # os.system(cmd)
+            # cmd = "yodamerge results/hej-output/HEJ*MuR2_MuF1* -o results/merged/HEJ-MUR2-MUF1.yoda"
+            # os.system(cmd)
+            # cmd = "yodamerge results/hej-output/HEJ*MuR0.5_MuF1* -o results/merged/HEJ-MUR0.5-MUF1.yoda"
+            # os.system(cmd)
+            # cmd = "yodamerge results/hej-output/HEJ*MuR1_MuF0.5* -o results/merged/HEJ-MUR1-MUF0.5.yoda"
+            # os.system(cmd)
+            cmd = "yodamerge results/hej-output/HEJ*MuR0.5_MuF0.5* -o results/merged/HEJ-MUR0.5-MUF0.5.yoda"
+            os.system(cmd)
+        
+        cmd = "rm results/hej-output/HEJ*Mu*"
+        os.system(cmd)
+        cmd = "yodamerge results/hej-output/HEJ* -o results/merged/HEJ.yoda"
+        os.system(cmd)
+        cmd = "rm -r results/hej-output"
+        os.system(cmd)
+        print("HEJ yoda files merged")
+
+        # Merge HEJ+Pythia results
+        print("Merging HEJ+Pythia yoda files")
+        cmd = "yodamerge results/hej-pythia-output/HEJ* -o results/merged/HEJmerging.yoda"
+        os.system(cmd)
+        cmd = "rm -r results/hej-pythia-output"
+        os.system(cmd)
+        print("HEJ+Pythia yoda files merged")
+
+
+    def clear_files(self):
+        """
+        Removes files created in scratch.
+        """
+        cmd = "rm -r %s" % (self.scratch_dir)
+        os.system(cmd)
+
+
+    def prune_output(self):
+        """
+        Performs a statistical analysis of the output
+        """
+        if not self.prune:
+            raise(ValueError("Pruning has not been set."))
+
+        cmd = str(self.prune_script)
+        os.system(cmd)
+
+
+def parse():
+    """
+    Parse command line arguments.
+        user_name : str user handle on gridui and dpm storage
+        job_number : int identifying submission
+        runs_per_job : int number of runs per submission < 4
+        events : int number of events per run
+        base_dir : base directory containing run configuration files
+        rivet_dir : directory containing rivet analyses
+        grid_output_dir : directory on grid storage for output, with protocol
+        name : job name
+    """
+    parser = argparse.ArgumentParser(description = "Usage: python run_hejpythia.py -u user_name -j job_number -p runs_per_job -e events -b base_dir -r rivet_dir -o grid_output_dir")
+    parser.add_argument('--user_name', '-u', nargs = 1, type = str)
+    parser.add_argument('--job_number', '-j', nargs = 1, type = int, default = 1)
+    parser.add_argument('--processes', '-p', nargs = 1, type = int, default = 1)
+    parser.add_argument('--events', '-e', nargs = 1, type = int, default = 100)
+    parser.add_argument('--base_dir', '-b', nargs = 1, type = str, default = os.getcwd())
+    parser.add_argument('--rivet_dir', '-r', nargs = 1, type = str, default = os.getcwd())
+    parser.add_argument('--output', '-o', nargs = 1, type = str)
+    return parser.parse_args()
+
+
+def main():
+    """
+    Run multiple HEJ+Pythia jobs per submission node.
+    """
+    args = parse()
+
+    t0 = time.time()
+    hejpythia = HejFogPythiaJob(args.user_name[0], args.job_number[0], args.base_dir[0], args.rivet_dir[0], args.output[0])
+    hejpythia.set_env()
+
+    if args.processes[0] > 4:
+        raise(ValueError("Maximum number of processes is 4 per node."))
+
+    t1 = time.time()
+    jobs = []
+    for number in range(args.processes[0]):
+        p = multiprocessing.Process(target = hejpythia.run_job, args = (number, args.events[0]))
+        jobs.append(p)
+
+    for job in jobs:
+        job.start()
+    
+    for job in jobs:
+        job.join()
+
+    t2 = time.time()
+
+    print("Environment setting time %s(s)" % (t1 - t0))
+    print("Execution time %s(s)" % (t2 - t1))
+    print("Total time %s(s)" % (t2 - t0))
+
+
+if __name__ == """__main__""":
+    main()
