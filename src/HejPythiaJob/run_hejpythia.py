9c382e3523c05b692ffd3ed8b976585969261838
Hitham <hithamh@live.com>
2023-12-17 21:07:46 +0000

    Add grid_base_dir to arguments for HejPythiaJob

--- src/HejPythiaJob/run_hejpythia.py
+++ src/HejPythiaJob/run_hejpythia.py
@@ -11,9 +11,7 @@ import multiprocessing
 class HejPythiaJob(): 
 
 
-    grid_base_dir = "gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/"
-
-    def __init__(self, user_name, job_number, base_dir, rivet_dir, output_dir):
+    def __init__(self, user_name, job_number, base_dir, rivet_dir, output_dir, grid_base_dir):
         """
         Initialises a HEJ+Pythia run given:
             user_name : str user name for gridui and dpm grid storage
@@ -21,12 +19,14 @@ class HejPythiaJob():
             base_dir : base directory for input files
             rivet_dir : path for compiled rivet analysis libraries, and PDFs
             output_dir : output directory on grid storage server, with protocol
+            grid_base_dir : location of HEP tools on grid storage server, with protocol
         """
         self.user_name = str(user_name)
         self.job_number = int(job_number)
         self.base_dir = str(base_dir)
         self.rivet_dir = str(rivet_dir)
         self.output_dir = str(output_dir)
+        self.grid_base_dir = str(grid_base_dir)
 
 
     def __del__(self):
@@ -51,7 +51,7 @@ class HejPythiaJob():
 
         print("Copying Sherpa.tar.gz from grid storage")
         os.system("mkdir ./Sherpa")
-        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/Sherpa/Sherpa.tar.gz . -f" % self.user_name
+        cmd = "gfal-copy %s/Sherpa/Sherpa.tar.gz . -f" % self.grid_base_dir
         os.system(cmd)
         os.environ["RIVET_ANALYSIS_PATH"] = str(self.rivet_dir)
         os.environ["LHAPDF_DATA_PATH"] = str(self.rivet_dir)
@@ -66,7 +66,7 @@ class HejPythiaJob():
         os.environ["SHERPA_LIBRARY_PATH"] = "%s/Sherpa/lib/SHERPA-MC" % str(os.getcwd())
         
         print("Copying libSherpaLHEfix.tar.gz from grid storage")
-        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/lib/libSherpaLHEfix.tar.gz ./ -f" % self.user_name
+        cmd = "gfal-copy %s/lib/libSherpaLHEfix.tar.gz ./ -f" % self.grid_base_dir
         os.system(cmd)
         print("untarring libSherpaLHEfix.tar.gz")
         os.system("tar -xzf libSherpaLHEfix.tar.gz")
@@ -78,7 +78,7 @@ class HejPythiaJob():
         os.environ["LD_LIBRARY_PATH"] = "%s:%s" % (str(os.environ.get("SHERPA_LHEFIX_PATH",'')), str(os.environ.get("LD_LIBRARY_PATH",'')))
 
         print("Downloading HEJ.tar.gz from grid storage")
-        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/HEJ/HEJ.tar.gz ./ -f" % self.user_name
+        cmd = "gfal-copy %s/HEJ/HEJ.tar.gz ./ -f" % self.grid_base_dir
         os.system(cmd)
         print("untarring HEJ.tar.gz")
         os.system("tar -xzf HEJ.tar.gz")
@@ -90,14 +90,14 @@ class HejPythiaJob():
         os.environ["PATH"] = "%s/HEJ/bin:%s" % (str(os.getcwd()), str(os.environ.get("PATH",'')))
 
         print("Downloading Pythia.tar.gz from grid storage")
-        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/Pythia/Pythia.tar.gz ./ -f" % self.user_name
+        cmd = "gfal-copy %s/Pythia/Pythia.tar.gz ./ -f" % self.grid_base_dir
         os.system(cmd)
         print("untarring Pythia.tar.gz")
         os.system("tar -xzf Pythia.tar.gz")
         os.system("rm -f Pythia.tar.gz")
         
         print("Downloading HEJ_pythia.tar.gz from grid storage")
-        cmd = "gfal-copy gsiftp://se01.dur.scotgrid.ac.uk/dpm/dur.scotgrid.ac.uk/home/pheno/%s/HEJ_pythia/HEJ_pythia.tar.gz ./ -f" % self.user_name
+        cmd = "gfal-copy %s/HEJ_pythia/HEJ_pythia.tar.gz ./ -f" % self.grid_base_dir
         os.system(cmd)
         print("untarring HEJ_pythia.tar.gz")
         os.system("tar -xzf HEJ_pythia.tar.gz")
@@ -383,9 +383,10 @@ def parse():
         base_dir : base directory containing run configuration files
         rivet_dir : directory containing rivet analyses
         grid_output_dir : directory on grid storage for output, with protocol
+        grid_base_dir : directory on grid storage for HEP tools storage, with protocol
         name : job name
     """
-    parser = argparse.ArgumentParser(description = "Usage: python run_hejpythia.py -u user_name -j job_number -p runs_per_job -e events -b base_dir -r rivet_dir -o grid_output_dir")
+    parser = argparse.ArgumentParser(description = "Usage: python run_hejpythia.py -u user_name -j job_number -p runs_per_job -e events -b base_dir -r rivet_dir -o grid_output_dir -g grid_base_dir")
     parser.add_argument('--user_name', '-u', nargs = 1, type = str)
     parser.add_argument('--job_number', '-j', nargs = 1, type = int, default = 1)
     parser.add_argument('--processes', '-p', nargs = 1, type = int, default = 1)
@@ -393,6 +394,7 @@ def parse():
     parser.add_argument('--base_dir', '-b', nargs = 1, type = str, default = os.getcwd())
     parser.add_argument('--rivet_dir', '-r', nargs = 1, type = str, default = os.getcwd())
     parser.add_argument('--output', '-o', nargs = 1, type = str)
+    parser.add_argument('--grid_base_dir', '-g', nargs = 1, type = str)
     return parser.parse_args()
 
 
@@ -403,7 +405,7 @@ def main():
     args = parse()
 
     t0 = time.time()
-    hejpythia = HejPythiaJob(args.user_name[0], args.job_number[0], args.base_dir[0], args.rivet_dir[0], args.output[0])
+    hejpythia = HejPythiaJob(args.user_name[0], args.job_number[0], args.base_dir[0], args.rivet_dir[0], args.output[0], args.grid_base_dir[0])
     hejpythia.set_env()
 
     if args.processes[0] > 4:
